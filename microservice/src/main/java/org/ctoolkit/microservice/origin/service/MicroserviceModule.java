eed6be03874cecb42fcbd89e5a449303d875f56f
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2017-12-12 10:16:14 +0100

    ObjectMapper (fasterxml jackson) Provider configuration has been added.

--- microservice/src/main/java/org/ctoolkit/microservice/origin/service/MicroserviceModule.java
+++ microservice/src/main/java/org/ctoolkit/microservice/origin/service/MicroserviceModule.java
@@ -1,6 +1,13 @@
 package org.ctoolkit.microservice.origin.service;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+import javax.inject.Singleton;
 
 /**
  * The application injection configuration.
@@ -13,5 +20,18 @@ public class MicroserviceModule
     @Override
     protected void configure()
     {
+    }
+
+    @Provides
+    @Singleton
+    ObjectMapper provideObjectMapper()
+    {
+        JsonFactory factory = new JsonFactory();
+        factory.enable( JsonParser.Feature.ALLOW_COMMENTS );
+
+        ObjectMapper mapper = new ObjectMapper( factory );
+        mapper.setSerializationInclusion( JsonInclude.Include.NON_NULL );
+
+        return mapper;
     }
 }
