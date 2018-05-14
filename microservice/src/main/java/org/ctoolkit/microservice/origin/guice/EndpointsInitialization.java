274fa87dc648d4ef28a4c66bbb758163b9f87057
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2018-05-14 07:17:18 +0200

    Multibindings are now in the Guice core artifact (from guice:4.2.0) so as a standalone dependency has been removed + dependency consolidation.

--- microservice/src/main/java/org/ctoolkit/microservice/origin/guice/EndpointsInitialization.java
+++ microservice/src/main/java/org/ctoolkit/microservice/origin/guice/EndpointsInitialization.java
@@ -2,11 +2,8 @@ package org.ctoolkit.microservice.origin.guice;
 
 import com.google.api.server.spi.ServletInitializationParameters;
 import com.google.api.server.spi.guice.EndpointsModule;
-import com.google.api.server.spi.guice.GuiceEndpointsServlet;
-import com.google.api.server.spi.guice.ServiceMap;
 import com.googlecode.objectify.ObjectifyFilter;
 import org.ctoolkit.microservice.origin.api.MessageEndpoint;
-import org.ctoolkit.services.endpoints.EndpointsContextAwareServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +13,6 @@ import static org.ctoolkit.services.endpoints.EndpointsMonitorConfig.ENDPOINTS_S
 
 /**
  * The endpoints service classes configuration.
- * As an alternative you can use context aware servlet {@link EndpointsContextAwareServlet}
- * instead of standard {@link GuiceEndpointsServlet}
  *
  * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
  * @see EndpointsModule
@@ -33,25 +28,11 @@ public class EndpointsInitialization
         ServletInitializationParameters params = ServletInitializationParameters.builder()
                 // add your endpoint service implementation
                 .addServiceClass( MessageEndpoint.class )
-                .setRestricted( true )
                 .setClientIdWhitelistEnabled( true ).build();
 
         configureEndpoints( ENDPOINTS_SERVLET_PATH, params );
 
         bind( ObjectifyFilter.class ).in( Singleton.class );
         filter( "/*" ).through( ObjectifyFilter.class );
-    }
-
-    protected void configureEndpoints( String urlPattern,
-                                       ServletInitializationParameters initParameters,
-                                       boolean useLegacyServlet )
-    {
-        bind( ServiceMap.class ).toInstance( ServiceMap.create( binder(), initParameters.getServiceClasses() ) );
-        if ( useLegacyServlet )
-        {
-            logger.error( "the legacy servlet is no longer available." );
-        }
-
-        super.serve( urlPattern ).with( GuiceEndpointsServlet.class, initParameters.asMap() );
     }
 }
